04d65d566cd6b00c485f0f53ddfc183f9d9f81c9
Giorgos <mayshmaz@gmail.com>
2018-11-14 17:56:35 +0200

    test fixed, no path test added

--- src/test/java/com/gio/chess/path/PiecePathCalculatorTest.java
+++ src/test/java/com/gio/chess/path/PiecePathCalculatorTest.java
@@ -1,13 +1,12 @@
 package com.gio.chess.path;
 
 import com.gio.chess.path.exceptions.IncorrectPositionException;
+import com.gio.chess.path.model.Knight;
 import com.gio.chess.path.model.Path;
 import com.gio.chess.path.model.Position;
-import com.gio.chess.path.model.Knight;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
 import java.util.Set;
 
 public class PiecePathCalculatorTest {
@@ -18,5 +17,31 @@ public class PiecePathCalculatorTest {
         Position end = new Position("e6");
         Set<Path> paths = knight.getPaths(3, end);
         Assert.assertEquals(3, paths.size());
+        Path expectedPath1 = (new Path(new Position("a1")))
+                .copyAndAppend(new Position("b3"))
+                .copyAndAppend(new Position("c5"))
+                .copyAndAppend(new Position("e6"));
+        paths.contains(expectedPath1);
+        Assert.assertTrue(paths.contains(expectedPath1));
+        Path expectedPath2 = (new Path(new Position("a1")))
+                .copyAndAppend(new Position("b3"))
+                .copyAndAppend(new Position("d4"))
+                .copyAndAppend(new Position("e6"));
+        Assert.assertTrue(paths.contains(expectedPath2));
+        Path expectedPath3 = (new Path(new Position("a1")))
+                .copyAndAppend(new Position("c2"))
+                .copyAndAppend(new Position("d4"))
+                .copyAndAppend(new Position("e6"));
+        Assert.assertTrue(paths.contains(expectedPath3));
     }
+
+    @Test
+    public void pathCalculationNoPathTest() throws IncorrectPositionException {
+        Knight knight= new Knight(new Position("a1"));
+        Position end = new Position("c5");
+        Set<Path> paths = knight.getPaths(3, end);
+        Assert.assertEquals(0, paths.size());
+
+    }
+
 }

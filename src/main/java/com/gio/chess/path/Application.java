283aea00ba2f05134cd9232535f54ab51492de85
Giorgos <mayshmaz@gmail.com>
2018-11-15 17:01:38 +0200

    input for piece type added

--- src/main/java/com/gio/chess/path/Application.java
+++ src/main/java/com/gio/chess/path/Application.java
@@ -5,6 +5,7 @@ import com.gio.chess.path.calculation.SimplePathCalculationTemplate;
 import com.gio.chess.path.exceptions.IncorrectPositionException;
 import com.gio.chess.path.model.Path;
 import com.gio.chess.path.model.Position;
+import com.gio.chess.path.moves.KingMovesStrategy;
 import com.gio.chess.path.moves.KnightMovesStrategy;
 import com.gio.chess.path.moves.PieceMovesStrategy;
 
@@ -28,7 +29,19 @@ public class Application {
 
             int steps = Integer.parseInt(scanner.nextLine());
 
-            PieceMovesStrategy pieceMovesStrategy = new KnightMovesStrategy();
+            PieceMovesStrategy pieceMovesStrategy;
+
+            System.out.println("Please enter 1 to get paths for Knight, 2 to get paths for King");
+
+            int selectedPiece = Integer.parseInt(scanner.nextLine());
+            if(selectedPiece == 1)
+                pieceMovesStrategy  = new KnightMovesStrategy();
+            else if (selectedPiece == 2)
+                pieceMovesStrategy  = new KingMovesStrategy();
+            else {
+                System.out.println("Incorrect piece type");
+                return;
+            }
             PathCalculationTemplate pathCalculationTemplate = new SimplePathCalculationTemplate(pieceMovesStrategy);
             Set<Path> paths = pathCalculationTemplate.getPaths(steps, new Position(start), new Position(end));
             if(paths.isEmpty()){
